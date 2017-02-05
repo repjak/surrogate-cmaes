5fc343aea0ece07820f288de07fe859bf569933e
Zbynek Pitra <jdgregorian@seznam.cz>
2017-02-06 00:20:42 +0100

    modelTesting_ordgp_02 bugfix

--- exp/experiments/modelTesting_ordgp_02.m
+++ exp/experiments/modelTesting_ordgp_02.m
@@ -1,12 +1,12 @@
 % ordgp model testing
 
 % settings
-func = 1;%(1:24);
-dims = 2;%[2, 5, 10, 20];
+func = (1:24);
+dims = [2, 5, 10, 20];
 maxEvals = 100;
 
 % path settings
-defFolder = fullfile('exp', 'experiments', 'model2');
+defFolder = fullfile('exp', 'experiments', 'model');
 defSetFile = fullfile(defFolder, 'defData', ['defSet_', num2str(maxEvals), 'FE.mat']);
 defModelFolder = fullfile(defFolder, 'defData', ['defModel_', num2str(maxEvals), 'FE']);
 
